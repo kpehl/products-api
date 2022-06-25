2061dcac1ad4bbf4ea9968ec38e742e4a95d4f9d
Kathleen Pehl <kathleen.pehl@gmail.com>
2022-06-25 11:29:03 -0400

    installed swagger-ui-express; documented v1 API get all and create Product routes and verified functionality of /docs in browser as well as testing documented routes in the swagger docs

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./controllers');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.use(
+  '/docs',
+  swaggerUi.serveFiles(swaggerDoc),
+  swaggerUi.setup(swaggerDoc)
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
