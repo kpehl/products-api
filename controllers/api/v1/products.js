8bb74fedda06340ebd7cadd8bc840b9f5101cfde
Kathleen Pehl <kathleen.pehl@gmail.com>
2022-06-26 10:55:07 -0400

    refactored v1 api to still parse stock field correctly as new quantity column; verified routes in Swagger

--- controllers/api/v1/products.js
+++ controllers/api/v1/products.js
@@ -5,7 +5,14 @@ router.get('/', async (req, res) => {
   try {
     const { rows } = await Product.getAll(req.query);
 
-    res.status(200).json(rows);
+    const stockedRows = rows.map((row) => {
+      return {
+        ...row,
+        stock: row.quantity
+      }
+    });
+
+    res.status(200).json(stockedRows);
   }
   catch (err) {
     console.error(err);
@@ -15,7 +22,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const { rows } = await Product.create(req.body);
+    const { rows } = await Product.create({
+      ...req.body,
+      quantity: req.body.stock
+    });
 
     res.status(200).json(rows[0]);
   }
@@ -32,7 +42,12 @@ router.get('/:id', async (req, res) => {
     });
 
     if (rowCount > 0) {
-      res.status(200).json(rows[0]);
+      const row = {
+        ...rows[0],
+        stock: rows[0].quantity
+      }
+
+      res.status(200).json(row);
     }
     else {
       res.status(404).end();
@@ -48,7 +63,8 @@ router.put('/:id', async (req, res) => {
   try {
     const { rowCount } = await Product.update({ 
       id: req.params.id,
-      ...req.body
+      ...req.body,
+      quantity: req.body.stock
     });
 
     res.status(rowCount === 0 ? 404 : 204).end();
